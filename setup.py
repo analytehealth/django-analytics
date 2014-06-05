f5361bc29963586c0cb2b96cf23ff84b00fdd302
Val Kolovos <val.kolovos@analytehealth.com>
2014-06-05 13:08:37 -0500

    needed to specify package_data in setup.py for it to show up in the installed directory

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ setup(
     url='https://github.com/analytehealth/django-analytics',
     download_url='https://github.com/analytehealth/django-analytics/archive/0.1.tar.gz',
     packages=find_packages(exclude=('*.tests',)),
+    package_data={
+        'djanalytics': [
+            'templates/charts/*',
+            'templates/*png'
+        ],
+    },
     description='Django app to capture, track and display site analytics',
     long_description=open('README.md').read(),
     install_requires=[
