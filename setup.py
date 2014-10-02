04e49ba18fcff190af4d50243788ceeab6d99348
Val Kolovos <val.kolovos@analytehealth.com>
2014-10-02 15:58:03 -0500

    set version to 0.11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     from setuptools import setup, find_packages
 from setuptools_utils import minify
 
-version = '0.11.dev'
+version = '0.11'
 
 setup(
     name='dj-analytics',
