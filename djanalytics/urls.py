7e35bca9f2f3374e070464068d29a6f754d5563d
Val Kolovos <val.kolovos@analytehealth.com>
2014-10-22 19:32:01 -0500

    adding framework for reports

--- djanalytics/urls.py
+++ djanalytics/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = patterns('djanalytics.views',
     url(r'^capture/$', 'capture_event', name='dja_capture'),
     url(r'^djanalytics.js$', 'djanalytics_js', name='djanalytics_js'),
+    url(r'^$', TemplateView.as_view(template_name='reports/djanalytics_index.html')),
 )
