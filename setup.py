d187e163c1b7e8629ad9fe9c08141d8590847693
Ron Hsiao <ronald.hsiao@analytemedia.com>
2016-04-21 16:42:20 -0500

    TP #7667 - Changes version number to 1.0.6-beta

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     from setuptools import setup, find_packages
 from setuptools_utils import minify
 
-version = '1.0.6'
+version = '1.0.6-beta'
 
 setup(
     name='dj-analytics',
